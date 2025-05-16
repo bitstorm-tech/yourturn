4da15bc6997595fee1c67041ba7a69b337379c16
Josef Bauer <bauer.josef1@swm.de>
2025-05-16 16:48:22 +0200

    Improve top navbar

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,7 +9,7 @@
         @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
 
-    <body>
+    <body class="bg-base-content">
         <livewire:top-nav-bar />
         {{ $slot }}
     </body>
